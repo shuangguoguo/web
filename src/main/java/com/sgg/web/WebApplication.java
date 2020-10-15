2dd1d4c931560d6d6f752e325edf325ad9b309bb
songshuang <shuangguoguo@vip.sina.com>
2020-10-15 17:45:15 +0800

    update file

--- src/main/java/com/sgg/web/WebApplication.java
+++ src/main/java/com/sgg/web/WebApplication.java
@@ -3,15 +3,24 @@ package com.sgg.web;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan("com.sgg.web.mapper")
 @EnableSwagger2
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 
-}
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+            // 注意这里要指向原先用main方法执行的Application启动类
+            return builder.sources(WebApplication.class);
+        }
+
+
+    }
